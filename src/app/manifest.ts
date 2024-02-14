15e780085b4d9f7ad0feb95be67f904b964f6944
kirylshaliapin <kiryl.shaliapin@gmail.com>
2024-02-14 19:21:12 +0300

    manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Kiryl Shalipain portfolio web',
+    short_name: 'Portfolio',
+    description: 'Portfolio web site for recruiters. Here you will find information about Kiryl Shalipain',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: `/favicon.ico`,
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ]
+  }
+}
