a0e52cdbd27100291ce0733b590150a7d6bd43a7
kirylshaliapin <kiryl.shaliapin@itrexgroup.com>
2024-02-15 20:32:54 +0300

    form

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,23 @@
+import {useForm} from 'react-hook-form'
+import {z}  from 'zod'
+
+enum FieldNames {
+  FULLNAME = 'fullname'
+}
+
+const schema = z.object({
+  [FieldNames.FULLNAME]: z.string()
+})
+
+type FormFields = z.infer<typeof schema>
+
 export const Contact = () => {
+  const {register} = useForm<FormFields>()
   return (
     <div className="p-[32px]">
-      Contact form
+      <form action="">
+        <input type="text" {...register(FieldNames.FULLNAME)} />
+      </form>
     </div>
   )
 }
