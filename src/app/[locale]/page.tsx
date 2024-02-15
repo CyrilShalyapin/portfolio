b8727192b65c05db21c7597c74fa73e5fca30858
kirylshaliapin <kiryl.shaliapin@itrexgroup.com>
2024-02-15 19:58:04 +0300

    navbar init

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,12 @@
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
+import { Navbar } from '../../components';
 
 export default function Home() {
   const t = useTranslations();
   return (
     <main>
       <div className="w-screen h-screen bg-[url('/hero-landscape.jpg')] bg-cover">
+        <Navbar />
         <div className='w-screen flex items-center justify-center'>
           <h2 className='text-[36px] text-[#616161] mt-[20%]'>𝕥𝕙𝕖 𝕟𝕒𝕥𝕦𝕣𝕖 𝕠𝕗 𝕔𝕠𝕕𝕖</h2>
         </div>
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <div className="">
+      <ul className="w-full flex gap-[16px] p-[16px]">
+        <li>LOGO</li>
+        <li>About me</li>
+        <li>Experience</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+  )
+}
