bd7ea761f0e23c89b179f2fd1763510e4fcc6ad0
kirylshaliapin <kiryl.shaliapin@gmail.com>
2024-02-16 21:52:29 +0300

    contact us redo

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,34 +1,12 @@
-'use client'
-
-import { zodResolver } from '@hookform/resolvers/zod'
-import {useForm} from 'react-hook-form'
-import {z}  from 'zod'
-
-enum FieldNames {
-  FULLNAME = 'fullname',
-  MESSAGE = 'message',
-}
-
-const schema = z.object({
-  [FieldNames.FULLNAME]: z.string(),
-  [FieldNames.MESSAGE]: z.string(),
-})
-
-type FormFields = z.infer<typeof schema>
-
 export const Contact = () => {
-  const {register} = useForm<FormFields>({
-    resolver: zodResolver(schema)
-  })
   return (
     <div className="p-[32px]">
-      <p>FORM</p>
-      <form>
-        <label htmlFor={FieldNames.FULLNAME}>Fullname</label>
-        <input type="text" {...register(FieldNames.FULLNAME)} />
-        <label htmlFor={FieldNames.MESSAGE}>Message</label>
-        <input type="textarea" {...register(FieldNames.MESSAGE)} />
-      </form>
+      <p>Come say hi</p>
+      <ul>
+        <li>Linked in</li>
+        <li>github</li>
+        <li>gmail</li>
+      </ul>
     </div>
   )
 }
